620efde9a08a975b5d0ddb07f0344f6231649f8e
rykugur <rollhax@gmail.com>
2022-11-16 23:15:06 -0600

    Update gradle dependencies; update manifest; add application class with hilt annotation and timber debug tree

---
+++ app/src/main/java/com/example/livefrontcodechallenge/ChallengeApplication.kt
@@ -0,0 +1,18 @@
+package com.example.livefrontcodechallenge
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
+
+@HiltAndroidApp
+class ChallengeApplication : Application() {
+
+  override fun onCreate() {
+    super.onCreate()
+
+    if (BuildConfig.DEBUG) {
+      Timber.plant(Timber.DebugTree())
+    }
+    // TODO: crashlytics tree?
+  }
+}
